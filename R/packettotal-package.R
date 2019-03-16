409715fc9760a8c5ff74aea0251fae1cfd00044b
hrbrmstr <bob@rud.is>
2019-03-16 14:45:27 -0400

    R package repo initialization complete

---
+++ R/packettotal-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/packettotal>
+#' - BugReports: <https://gitlab.com/hrbrmstr/packettotal/issues>
+#' 
+#' @md
+#' @name packettotal
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
